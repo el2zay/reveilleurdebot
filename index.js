fe2484c4a1f3c43c133343ef4735681be5fef40a
Elie404 <79168733+Elie404@users.noreply.github.com>
2021-06-12 20:59:54 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,60 @@
+let Discord;
+let Database;
+if (typeof window !== "undefined") {
+    Discord = DiscordJS;
+    Database = EasyDatabase;
+} else {
+    Discord = require("discord.js");
+    Database = require("easy-json-database");
+}
+const delay = (ms) => new Promise((resolve) => setTimeout(() => resolve(), ms));
+const s4d = {
+    Discord,
+    client: null,
+    tokenInvalid: false,
+    reply: null,
+    joiningMember: null,
+    database: new Database("./db.json"),
+    checkMessageExists() {
+        if (!s4d.client) throw new Error('You cannot perform message operations without a Discord.js client')
+        if (!s4d.client.readyTimestamp) throw new Error('You cannot perform message operations while the bot is not connected to the Discord API')
+    }
+};
+s4d.client = new s4d.Discord.Client({
+    fetchAllMembers: true
+});
+s4d.client.on('raw', async (packet) => {
+    if (['MESSAGE_REACTION_ADD', 'MESSAGE_REACTION_REMOVE'].includes(packet.t)) {
+        const guild = s4d.client.guilds.cache.get(packet.d.guild_id);
+        if (!guild) return;
+        const member = guild.members.cache.get(packet.d.user_id) || guild.members.fetch(d.user_id).catch(() => {});
+        if (!member) return;
+        const channel = s4d.client.channels.cache.get(packet.d.channel_id);
+        if (!channel) return;
+        const message = channel.messages.cache.get(packet.d.message_id) || await channel.messages.fetch(packet.d.message_id).catch(() => {});
+        if (!message) return;
+        s4d.client.emit(packet.t, guild, channel, message, member, packet.d.emoji.name);
+    }
+});
+s4d.client.login('XffvZtnmagcjXDi2xK92FCk0ix8OyuWW').catch((e) => {
+    s4d.tokenInvalid = true;
+    s4d.tokenError = e;
+});
+
+s4d.client.on('message', async (s4dmessage) => {
+    if ((s4dmessage.content) == 'e!reveil' && (s4dmessage.channel) == s4d.client.channels.cache.get('852914968436408362')) {
+
+        while (s4d.client && s4d.client.token) {
+            await delay(50);
+            s4dmessage.channel.send(String('e!test'));
+            s4dmessage.channel.send(String('win ping'));
+            s4dmessage.channel.send(String('RÉVEILLEZ VOUS '));
+            await delay(Number(250) * 1000);
+
+            console.log('ran')
+        }
+    }
+
+});
+
+s4d;
